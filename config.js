3c1b7c7b9a2ce3c3f452476acf96508e3bb9dccb
alex-voytko <voytko1994@gmail.com>
2022-04-12 23:02:51 +0300

    make it better

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  secret: "SECRET_KEY_RANDOM",
+  db_url: `mongodb+srv://alex-voytko:123121cd@cluster0.7lrvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  port: 5000,
+};
